79aaeba88ee07f69395cd7d2faf2b59476e84400
William Reima <william.reima@gmail.com>
2025-01-27 13:31:54 +0200

    MQTT server port forward automation functional

--- MQTT/mqtt_config.sh
+++ MQTT/mqtt_config.sh
@@ -6,7 +6,8 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 if ! command -v docker &>/dev/null; then
     echo "Docker is not installed..."
 
-    ./HAOS/docker_install.sh
+    #./HAOS/docker_install.sh
+    return 1
 fi
 
 # TODO:
@@ -15,12 +16,15 @@ uci add firewall redirect
 uci set firewall.@redirect[-1].name="Forward-MQTT-to-Docker"
 uci set firewall.@redirect[-1].src="wan"
 uci set firewall.@redirect[-1].src_dport="1883"
-uci set firewall.@redirect[-1].dest="docker"
+uci set firewall.@redirect[-1].dest="lan"
 uci set firewall.@redirect[-1].dest_port="1883"
 uci set firewall.@redirect[-1].proto="tcp"
 uci set firewall.@redirect[-1].target="DNAT"
 
 uci commit firewall
 /etc/init.d/firewall reload
+
+mkdir -p ./mosquitto/config/
+cp mosquitto.conf ./mosquitto/config/mosquitto.conf
 # Launch container
 docker-compose -f ./mqttcompose.yml up -d
