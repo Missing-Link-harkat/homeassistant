fa4935a5cfed8b737252aa19d6906a93987588af
William Reima <william.reima@gmail.com>
2025-02-20 12:57:06 +0200

    start of zerotier auth config

--- OpenWrt/network/zerotier/identification_make.sh
+++ OpenWrt/network/zerotier/identification_make.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # Create new public and private keypair for ZeroTier identity, enabling predefined whitelist
 
+# Check if the ZeroTier is installed
 if command -v zerotier-cli >/dev/null 2>&1; then
-    echo "<command-name> is installed"
+    echo "zerotier-cli is installed"
 else
-    echo "<command-name> is not installed"
+    echo "zerotier-cli is not installed"
 fi
+
+
