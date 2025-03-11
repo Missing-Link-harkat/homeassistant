2a937ac5bd4abdee2833b3d8b75464d7fab57a27
William Reima <william.reima@gmail.com>
2025-03-11 14:20:13 +0200

    mqtt auth

--- MQTT/mqtt_config.sh
+++ MQTT/mqtt_config.sh
@@ -28,3 +28,19 @@ mkdir -p ./mosquitto/config/
 cp mosquitto.conf ./mosquitto/config/mosquitto.conf
 # Launch container
 docker-compose -f ./mqttcompose.yml up -d
+
+# Configure container
+CONFIG_FILE="./mosquitto/config/mosquitto.conf"
+
+sed -i '/^allow_anonymous true/s/^/#/' "$CONFIG_FILE"   # Comment out 'allow_anonymous true'
+sed -i '/^#allow_anonymous false/s/^#//' "$CONFIG_FILE"  # Uncomment 'allow_anonymous false'
+sed -i '/^#password_file \/mosquitto\/config\/mosquitto.passwd/s/^#//' "$CONFIG_FILE"  # Uncomment 'password_file'
+
+
+# Setup initial test user
+echo "Setting up initial user to MQTT, please enter user password:"
+CONTAINER_NAME="mosquitto"
+PASSWORD_FILE="/mosquitto/config/mosquitto.passwd"
+INITIAL_USER="admin"
+
+docker exec -it ${CONTAINER_NAME} sh -c "mosquitto_passwd -c ${PASSWORD_FILE} ${INITIAL_USER}"
